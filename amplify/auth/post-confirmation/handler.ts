f929c833058732f9f47c3196e3d85f5cd63b37e9
IshikaSaxena <ishika.3.saxena@bt.com>
2025-12-07 13:02:48 +0530

    adding final stages

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,6 +1,52 @@
-// this is the code that runs in lambda function
-import type {PostConfirmationTriggerHandler} from "aws-lambda";
+import type { PostConfirmationTriggerHandler } from "aws-lambda";
+import { type Schema } from "../../data/resource";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { env } from "$amplify/env/post-confirmation";
+import { createUserProfile } from "./graphql/mutations";
 
-export const handler:PostConfirmationTriggerHandler = async (event) => {
-    return event;
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthMode: "iam",
+      },
+    },
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN,
+          },
+        }),
+        clearCredentialsAndIdentityId: () => {
+          /* noop */
+        },
+      },
+    },
+  }
+);
+
+const client = generateClient<Schema>({
+  authMode: "iam",
+});
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  await client.graphql({
+    query: createUserProfile,
+    variables: {
+      input: {
+        email: event.request.userAttributes.email,
+        profileOwner: `${event.request.userAttributes.sub}::${event.userName}`,
+      },
+    },
+  });
+
+  return event;
 };
